fee25e03b6dc6d585e05ae6ca77b813ba4ebd6c9
Richard Sanchez <richard.charles.s@gmail.com>
2018-05-29 19:37:55 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,7 @@
 def display_board(board = ["  ", "  ", "  ", "  ", " X ", "  ", "  ", "  ", "  "])
     puts "   |   |   "
     puts "-----------"
-    puts "   | X |   "
+    puts "   |   |   "
     puts "-----------"
     puts "   |   |   "
 end
